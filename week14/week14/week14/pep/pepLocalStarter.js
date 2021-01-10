581c21483b9b3efb5baee9175c6b37ad3f2dbdad
AndreasRohr <andreas.rohr@students.fhnw.ch>
2021-01-10 15:19:18 +0100

    folders

--- week14/week14/week14/pep/pepLocalStarter.js
+++
@@ -1,9 +0,0 @@
-
-import { pepServices } from "./service/localService.js";
-import { start }  from "./pep.js";
-
-const appRootId = window.appRootId;
-
-pepServices().loadDevelopers( devs => start(appRootId, devs) );
-
-
